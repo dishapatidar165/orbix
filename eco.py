b1c9829e5af179729d7d6fe250f7613fba7a40c4
Jayant755470 <Jayantc554@gmail.com>
2026-05-06 06:26:04 +0000

    hi

---
+++ eco.py
@@ -0,0 +1,11 @@
+class orgamism:
+    def __init__(self):
+        pass
+    def energy(self):
+        pass
+class plant(orgaism):
+class animal(orgaism):
+class carnivorus(animal):
+class harviborus(animal):
+class aqua_animal(animal):
+class birds(animal):
