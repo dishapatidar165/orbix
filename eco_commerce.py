bfe7e098a2f16f2ca096da91a8422de99a1445b1
Jayant755470 <Jayantc554@gmail.com>
2026-05-06 10:28:46 +0000

    first

---
+++ eco_commerce.py
@@ -0,0 +1,20 @@
+class Product:
+    def __init__(self, pid, name, price):
+        self.pid = pid
+        self.name = name
+        self.price = price
+
+    def display_product(self):
+        print(f"ID: {self.product_id}")
+        print(f"Name: {self.name}")
+        print(f"Price: ₹{self.price}")
+
+
+
+class PaymentProcessor:
+
+ class CreditCard(pPaymentProcesso):
+
+  class PayPal(PaymentProcesso):
+
+   class crypto(PaymentProcesso):
