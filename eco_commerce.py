9240092a8b4ebf04c88a5edb3ba02cb0ceffdb09
Jayant755470 <Jayantc554@gmail.com>
2026-05-06 10:42:30 +0000

    second

--- eco_commerce.py
+++ eco_commerce.py
@@ -10,11 +10,49 @@ class Product:
         print(f"Price: ₹{self.price}")
 
 
+class ShoppingCard:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+        print(f"{product.name} added to cart")
+
+    def remove_product(self, product_name):
+        for item in self.products:
+            if item.name == product_name:
+                self.products.remove(item)
+                print(f"{product_name} removed from cart")
+                return
+        print("Product not found")
+   
+    def show_cart(self):
+        print("\n===== SHOPPING CART =====")
+        if len(self.products) == 0:
+            print("Cart is empty")
+        else:
+            for item in self.products:
+                print(f"{item.name} - ₹{item.price}")
+
+    def total_price(self):
+        total = 0
+        for item in self.products:
+            total += item.price
+        return total
+
+
+class Coupon:
+   def __init__(self):
+   
+class Order:
+   def __init__(self):
 
 class PaymentProcessor:
+      def __init__(self):
+          pass
 
- class CreditCard(pPaymentProcesso):
+class CreditCard(PaymentProcessor):
 
-  class PayPal(PaymentProcesso):
+class PayPal(PaymentProcessor):
 
-   class crypto(PaymentProcesso):
+class crypto(PaymentProcessor):
